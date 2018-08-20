69feebb28683e32cedc86f4d5e2225546732563c
Jon Koldo Arteaga <401958+jkarteaga@users.noreply.github.com>
2018-08-20 21:31:03 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import './Footer.css'
 export default ({ globalSettings, socialSettings, navLinks }) => (
   <footer className='Footer'>
     <div className='container taCenter'>
-      <span>© 2017 All rights reserved.</span>
+      <span><a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Licencia de Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/80x15.png" /></a><br />Este obra está bajo una <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">licencia de Creative Commons Reconocimiento 4.0 Internacional</a>.</span>
     </div>
   </footer>
 )
